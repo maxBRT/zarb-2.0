47e8bb5e074629be987746f801065af8ddc974cd
maxBRT <bourret74@gmail.com>
2026-03-18 09:38:12 -0400

    Base auth set up + french local for errors

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
+Route::view('/register', 'auth.register')->name('register');
+Route::view('/login', 'auth.login')->name('login');
+
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::view('/dashboard', 'dashboard')->name('dashboard')->middleware('auth');
